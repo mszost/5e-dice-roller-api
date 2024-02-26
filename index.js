4ede69f3d49140e7f9840e509fe536a747288bc8
mszost <mszo@tuta.io>
2024-02-25 23:27:55 -0600

    fix CORS (maybe?)

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ app.use(express.static(__dirname + '/static'))
 const corsOptions = {
     // block requests from origins that do not match the following
     origin: "https://wonderful-tree-04bcf0610.4.azurestaticapps.net/",
+    methods: "GET",
+    allowedHeaders: 'Access-Control-Allow-Origin',
     optionsSuccessStatus: 200,
 }
 
@@ -20,7 +22,7 @@ app.use(cors(corsOptions))
 app.get('/', (req, res) => res.render('home'))
 
 // generate random number based on the "d" value passed by the url
-app.get('/roll', cors(corsOptions), (request, response) => {
+app.get('/roll', (request, response) => {
 	console.log('Calling "/roll".')
     var inputs = url.parse(request.url, true).query
     let roll = Math.floor(Math.random() * parseInt(inputs.d)) + 1
