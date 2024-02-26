d2980782d6ccf4829e40518a04ae461371d8859e
mszost <mszo@tuta.io>
2024-02-25 23:49:03 -0600

    attempt to fix CORS again

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const cors = require("cors")
-const port = process.env.PORT || 3000
+var url = require('url')
 
 const app = express()
 
-var url = require('url')
+const corsOptions = {
+    // block requests from origins that do not match the following
+    origin: "https://wonderful-tree-04bcf0610.4.azurestaticapps.net",
+    methods: "GET",
+    allowedHeaders: 'Access-Control-Allow-Origin',
+    optionsSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions))
+
 
 app.use(express.static(__dirname + '/static'))
 
@@ -12,7 +21,7 @@ app.get('/', (req, res) => res.render('home'))
 
 // generate random number based on the "d" value passed by the url
 app.get('/roll', (request, response) => {
-	console.log('Calling "/roll".')
+    console.log('Calling "/roll".')
     var inputs = url.parse(request.url, true).query
     let roll = Math.floor(Math.random() * parseInt(inputs.d)) + 1
     response.send(roll.toString())
@@ -31,7 +40,8 @@ app.use((err, request, response, next) => {
     response.send('500 - Server Error')
 })
 
+const port = process.env.PORT || 3000
 app.listen(port, () => console.log(
     `Express started at \"http://localhost:${port}\"\n` +
     `press Ctrl-C to terminate.`)
-)
+    )
